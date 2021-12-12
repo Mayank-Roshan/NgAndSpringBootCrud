6d5ad08428df78ea8c00304deee3bf0d26777345
Mayank-Roshan <mayankroshan24@gmail.com>
2021-12-12 22:26:47 +0530

    Mayank - Added add new and edit employee
    Create and edit employee functionalities done.
    Form resets after submit
    Closes on close, delete left to do

--- employeemanager-ui/src/app/app.component.ts
+++ employeemanager-ui/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { HttpErrorResponse } from '@angular/common/http';
+import { HttpErrorResponse, HttpStatusCode } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Employee } from './models/employee';
@@ -12,6 +12,7 @@ import { EmployeeService } from './service/employee.service';
 export class AppComponent implements OnInit {
   title = 'employeemanager-ui';
   employeeForm!: FormGroup;
+  employee!:Employee;
 
   ngOnInit(){
     this.getEmployees();
@@ -53,12 +54,57 @@ export class AppComponent implements OnInit {
     }
     else if(mode==='edit'){
       button.setAttribute('data-target','#updateEmployeeModal');
+      this.copyEmployeeToForm(employee);
+
     }
     else if(mode==='delete'){
       button.setAttribute('data-target','#deleteEmployeeModal');
     }
     container?.appendChild(button);
     button.click();
+  }
+
+  addEmployee():void{
+    const employee=this.employeeForm.value as Employee;
+    this.employeeService.addEmployees(employee).subscribe(
+      (response:Employee)=>{
+        console.log(response);
+        this.getEmployees();
+        this.resetEmployeeForm();
+      },
+      (error:HttpErrorResponse)=>{
+        console.log(error.message);
+      }
+    );
+  }
+
+  editEmployee():void{
+    const employee=this.employeeForm.value as Employee;
+    this.employeeService.updateEmployees(employee).subscribe(
+      (response:Employee)=>{
+        console.log(response);
+        this.getEmployees();
+        this.resetEmployeeForm();
+      },
+      (error:HttpErrorResponse)=>{
+        console.log(error.message);
+      }
+    );
+  }
+
+  resetEmployeeForm(){
+    this.employeeForm.reset();
+  }
+
+  copyEmployeeToForm(employee:any){
+    this.employeeForm.setValue({
+      name:employee.name,
+      imageUrl:employee.imageUrl,
+      jobTitle:employee.jobTitle,
+      phone:employee.phone,
+      email:employee.email,
+      id:employee.id
+    });
   }
 
 }
